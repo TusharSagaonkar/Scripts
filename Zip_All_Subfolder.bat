a7bca76e8a2568a1e237f033aee657bcacd9f9f7
TusharSagaonkar <tush.sagaonkar@gmail.com>
2024-11-07 14:26:38 +0530

    Update Zip_All_Subfolder.bat

--- Zip_All_Subfolder.bat
+++ Zip_All_Subfolder.bat
@@ -1,4 +1,7 @@
 @echo off
+:: Enable delayed expansion for variables inside loops
+setlocal enabledelayedexpansion
+
 :: Get the current directory where the batch file is run
 set "source_dir=%cd%"
 
@@ -7,9 +10,9 @@ for /D %%D in ("%source_dir%\*") do (
     :: Get the name of the subfolder
     set "folder_name=%%~nxD"
     :: Define the output zip file name in the same directory
-    set "zip_file=%source_dir%\%folder_name%.zip"
+    set "zip_file=%source_dir%\!folder_name!.zip"
     :: Use PowerShell to zip the subfolder
-    powershell -command "Compress-Archive -Path '%%D\*' -DestinationPath '%zip_file%'"
+    powershell -command "Compress-Archive -Path '%%D\*' -DestinationPath '!zip_file!'"
 )
 
 echo Zipping complete!
