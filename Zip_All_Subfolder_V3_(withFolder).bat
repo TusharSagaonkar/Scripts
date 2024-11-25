f8ded1940b291a91b08a75238e46cae70304513a
TusharSagaonkar <tush.sagaonkar@gmail.com>
2024-11-25 18:55:31 +0530

    Add files via upload

---
+++ Zip_All_Subfolder_V3_(withFolder).bat
@@ -0,0 +1,19 @@
+@echo off
+:: Enable delayed expansion for variables inside loops
+setlocal enabledelayedexpansion
+
+:: Get the current directory where the batch file is run
+set "source_dir=%cd%"
+
+:: Loop through each subfolder in the current directory
+for /D %%D in ("%source_dir%\*") do (
+    :: Get the name of the subfolder
+    set "folder_name=%%~nxD"
+    :: Define the output zip file name in the same directory
+    set "zip_file=%source_dir%\!folder_name!.zip"
+    :: Use PowerShell to zip the folder, including the folder itself
+    powershell -command "Compress-Archive -Path '%%D\' -DestinationPath '!zip_file!'"
+)
+
+echo Zipping complete!
+pause
