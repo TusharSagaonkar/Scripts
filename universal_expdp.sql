ed715ac3fcc7f585e016c06dc1e20edfd4c72725
TusharSagaonkar <tush.sagaonkar@gmail.com>
2025-02-15 13:45:14 +0530

    Update universal_expdp.sql

--- universal_expdp.sql
+++ universal_expdp.sql
@@ -24,13 +24,27 @@ set /p SCHEMA="Enter Schema Name (default: %SCHEMA%): "
 IF NOT DEFINED SCHEMA SET SCHEMA=SCOTT
 
 set /p TABLES="Enter Table(s) to Export (comma-separated or leave blank for full schema): "
+
+:: Determine filename suffix based on table count
 IF NOT DEFINED TABLES (
     SET MODE=SCHEMA
     SET FILE_SUFFIX=ALL
 ) ELSE (
     SET MODE=TABLE
-    SET FILE_SUFFIX=!TABLES!
-    SET FILE_SUFFIX=!FILE_SUFFIX:,=_!
+    FOR /F "tokens=1-4 delims=," %%A IN ("%TABLES%") DO (
+        IF "%%D"=="" (
+            SET FILE_SUFFIX=!TABLES!
+            SET FILE_SUFFIX=!FILE_SUFFIX:,=_!
+        ) ELSE (
+            FOR /F %%X IN ('cmd /c echo %TABLES% ^| find /c ","') DO SET /A TABLE_COUNT=%%X+1
+            IF !TABLE_COUNT! GTR 2 (
+                SET FILE_SUFFIX=TABLES(!TABLE_COUNT!)
+            ) ELSE (
+                SET FILE_SUFFIX=!TABLES!
+                SET FILE_SUFFIX=!FILE_SUFFIX:,=_!
+            )
+        )
+    )
 )
 
 set /p FILESIZE="Enter Dump File Size Limit (e.g., 5G, 10G) (default: %FILESIZE%): "
@@ -42,110 +56,27 @@ IF NOT DEFINED EXPORT_VERSION SET EXPORT_VERSION=LATEST
 set /p PARALLEL="Enter Parallel Workers (0 to disable parallelism) (default: %PARALLEL%): "
 IF NOT DEFINED PARALLEL SET PARALLEL=4
 
-:: Display Include/Exclude Options
-echo.
-echo Select objects to INCLUDE (leave blank to skip):
-echo 1. TABLE:EMP
-echo 2. INDEX
-echo 3. CONSTRAINT
-echo 4. VIEW
-echo 5. SEQUENCE
-echo 6. TRIGGER
-echo 7. PROCEDURE
-echo 8. FUNCTION
-echo 9. PACKAGE
-echo 10. PACKAGE BODY
-echo 11. TYPE
-echo 12. TYPE BODY
-echo 13. MATERIALIZED VIEW
-echo 14. SYNONYM
-echo 15. GRANT
-echo 16. STATISTICS
-echo 17. DB LINK
-echo (Use comma-separated values, e.g., 1,3,5 for TABLE:EMP, CONSTRAINT, SEQUENCE)
+:: Generate timestamp
+FOR /F "tokens=2 delims==" %%I IN ('wmic OS Get localdatetime /value') DO SET DATETIME=%%I
+SET DATETIME=%DATETIME:~0,8%_%DATETIME:~8,6%
 
-set /p INCLUDE_NUM="Enter numbers for INCLUDE (or leave blank): "
-IF "%INCLUDE_NUM%"=="" (
-    SET INCLUDE_PARAM=
+:: Generate dump and log file names
+SET DUMPFILE=EXPDP_%SCHEMA%_%FILE_SUFFIX%_%DATETIME%_%%U.dmp
+SET LOGFILE=EXPDP_%SCHEMA%_%FILE_SUFFIX%_%DATETIME%.log
+
+:: Running Data Pump Export
+SET EXPORT_CMD=expdp system/password@%ORACLE_SID% DIRECTORY=%DIRECTORY% DUMPFILE=%DUMPFILE% LOGFILE=%LOGFILE% FILESIZE=%FILESIZE% COMPRESSION=%COMPRESSION% VERSION=%EXPORT_VERSION%
+
+IF "%MODE%"=="SCHEMA" (
+    SET EXPORT_CMD=%EXPORT_CMD% SCHEMAS=%SCHEMA%
 ) ELSE (
-    SET INCLUDE=
-    FOR %%A IN (%INCLUDE_NUM%) DO (
-        IF "%%A"=="1" SET INCLUDE=!INCLUDE!,TABLE:EMP
-        IF "%%A"=="2" SET INCLUDE=!INCLUDE!,INDEX
-        IF "%%A"=="3" SET INCLUDE=!INCLUDE!,CONSTRAINT
-        IF "%%A"=="4" SET INCLUDE=!INCLUDE!,VIEW
-        IF "%%A"=="5" SET INCLUDE=!INCLUDE!,SEQUENCE
-        IF "%%A"=="6" SET INCLUDE=!INCLUDE!,TRIGGER
-        IF "%%A"=="7" SET INCLUDE=!INCLUDE!,PROCEDURE
-        IF "%%A"=="8" SET INCLUDE=!INCLUDE!,FUNCTION
-        IF "%%A"=="9" SET INCLUDE=!INCLUDE!,PACKAGE
-        IF "%%A"=="10" SET INCLUDE=!INCLUDE!,PACKAGE BODY
-        IF "%%A"=="11" SET INCLUDE=!INCLUDE!,TYPE
-        IF "%%A"=="12" SET INCLUDE=!INCLUDE!,TYPE BODY
-        IF "%%A"=="13" SET INCLUDE=!INCLUDE!,MATERIALIZED VIEW
-        IF "%%A"=="14" SET INCLUDE=!INCLUDE!,SYNONYM
-        IF "%%A"=="15" SET INCLUDE=!INCLUDE!,GRANT
-        IF "%%A"=="16" SET INCLUDE=!INCLUDE!,STATISTICS
-        IF "%%A"=="17" SET INCLUDE=!INCLUDE!,DB LINK
-    )
-    SET INCLUDE=!INCLUDE:~1!  :: Remove leading comma
-    SET INCLUDE_PARAM=INCLUDE=!INCLUDE!
+    SET EXPORT_CMD=%EXPORT_CMD% TABLES=%TABLES%
 )
 
-echo.
-echo Select objects to EXCLUDE (leave blank to skip):
-echo 1. TABLE:EMP
-echo 2. INDEX
-echo 3. CONSTRAINT
-echo 4. VIEW
-echo 5. SEQUENCE
-echo 6. TRIGGER
-echo 7. PROCEDURE
-echo 8. FUNCTION
-echo 9. PACKAGE
-echo 10. PACKAGE BODY
-echo 11. TYPE
-echo 12. TYPE BODY
-echo 13. MATERIALIZED VIEW
-echo 14. SYNONYM
-echo 15. GRANT
-echo 16. STATISTICS
-echo 17. DB LINK
-echo (Use comma-separated values, e.g., 2,6 for INDEX and TRIGGER)
-
-set /p EXCLUDE_NUM="Enter numbers for EXCLUDE (or leave blank): "
-IF "%EXCLUDE_NUM%"=="" (
-    SET EXCLUDE_PARAM=
-) ELSE (
-    SET EXCLUDE=
-    FOR %%B IN (%EXCLUDE_NUM%) DO (
-        IF "%%B"=="1" SET EXCLUDE=!EXCLUDE!,TABLE:EMP
-        IF "%%B"=="2" SET EXCLUDE=!EXCLUDE!,INDEX
-        IF "%%B"=="3" SET EXCLUDE=!EXCLUDE!,CONSTRAINT
-        IF "%%B"=="4" SET EXCLUDE=!EXCLUDE!,VIEW
-        IF "%%B"=="5" SET EXCLUDE=!EXCLUDE!,SEQUENCE
-        IF "%%B"=="6" SET EXCLUDE=!EXCLUDE!,TRIGGER
-        IF "%%B"=="7" SET EXCLUDE=!EXCLUDE!,PROCEDURE
-        IF "%%B"=="8" SET EXCLUDE=!EXCLUDE!,FUNCTION
-        IF "%%B"=="9" SET EXCLUDE=!EXCLUDE!,PACKAGE
-        IF "%%B"=="10" SET EXCLUDE=!EXCLUDE!,PACKAGE BODY
-        IF "%%B"=="11" SET EXCLUDE=!EXCLUDE!,TYPE
-        IF "%%B"=="12" SET EXCLUDE=!EXCLUDE!,TYPE BODY
-        IF "%%B"=="13" SET EXCLUDE=!EXCLUDE!,MATERIALIZED VIEW
-        IF "%%B"=="14" SET EXCLUDE=!EXCLUDE!,SYNONYM
-        IF "%%B"=="15" SET EXCLUDE=!EXCLUDE!,GRANT
-        IF "%%B"=="16" SET EXCLUDE=!EXCLUDE!,STATISTICS
-        IF "%%B"=="17" SET EXCLUDE=!EXCLUDE!,DB LINK
-    )
-    SET EXCLUDE=!EXCLUDE:~1!  
-    SET EXCLUDE_PARAM=EXCLUDE=!EXCLUDE!
-)
-
-:: Running expdp command with INCLUDE/EXCLUDE if selected
-SET EXPORT_CMD=expdp system/password@%ORACLE_SID% DIRECTORY=%DIRECTORY% DUMPFILE=EXPDP_%SCHEMA%_%FILE_SUFFIX%_%DATETIME%_%%U.dmp LOGFILE=EXPDP_%SCHEMA%_%FILE_SUFFIX%_%DATETIME%.log FILESIZE=%FILESIZE% COMPRESSION=%COMPRESSION% VERSION=%EXPORT_VERSION%
-
-IF NOT "%INCLUDE_PARAM%"=="" SET EXPORT_CMD=%EXPORT_CMD% %INCLUDE_PARAM%
-IF NOT "%EXCLUDE_PARAM%"=="" SET EXPORT_CMD=%EXPORT_CMD% %EXCLUDE_PARAM%
+IF NOT "%PARALLEL%"=="0" SET EXPORT_CMD=%EXPORT_CMD% PARALLEL=%PARALLEL%
 
 echo Running: %EXPORT_CMD%
 %EXPORT_CMD%
+
+echo Export Completed!
+PAUSE
