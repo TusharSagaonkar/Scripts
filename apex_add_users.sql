4f3d29a42cdaa385c149224bbd4ca2431884fe25
TusharSagaonkar <tush.sagaonkar@gmail.com>
2025-07-09 15:07:34 +0530

    Update apex_add_users.sql

--- apex_add_users.sql
+++ apex_add_users.sql
@@ -3,9 +3,9 @@ BEGIN
     APEX_UTIL.CREATE_USER(
       p_user_name     => rec.username,
       p_web_password  => rec.password,
-      p_email_address => rec.username,
-      p_developer_privs => NULL,             -- 👈 No dev rights (Reader only)
-      p_default_schema => 'YOUR_SCHEMA',     -- 🔁 Replace with your schema
+      p_first_name    => rec.fullname,        -- Full name stored here
+      p_developer_privs => NULL,              -- Reader only
+      p_default_schema => 'YOUR_SCHEMA',      -- 🔁 Replace with your schema
       p_change_password_on_first_use => 'N'
     );
   END LOOP;
