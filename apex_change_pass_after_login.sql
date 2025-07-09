c0d6bf310a2dcf769f522d6f0c73d38ea3d05cf1
TusharSagaonkar <tush.sagaonkar@gmail.com>
2025-07-09 16:36:39 +0530

    Update apex_change_pass_after_login.sql

--- apex_change_pass_after_login.sql
+++ apex_change_pass_after_login.sql
@@ -1,9 +1,19 @@
+DECLARE
+  v_workspace_id NUMBER;
 BEGIN
+  -- Set workspace context
+  SELECT workspace_id
+    INTO v_workspace_id
+    FROM apex_workspaces
+   WHERE workspace = 'YOUR_WORKSPACE_NAME'; -- Replace this
+
+  APEX_UTIL.SET_SECURITY_GROUP_ID(v_workspace_id);
+
+  -- Loop through your user list
   FOR rec IN (SELECT username FROM apex_user_list) LOOP
-    APEX_UTIL.SET_USER_ATTRIBUTE(
-      p_user_name => rec.username,
-      p_attribute_name => 'CHANGE_PASSWORD_ON_FIRST_USE',
-      p_attribute_value => 'Y'
+    APEX_UTIL.EDIT_USER(
+      p_user_name                    => rec.username,
+      p_change_password_on_first_use => 'Y'
     );
   END LOOP;
 END;
